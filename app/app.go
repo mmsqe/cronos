ae254f344097c181ef8d29e4dc40615f9e284940
adu-crypto <adu.du@crypto.com>
2022-10-25 14:56:49 +0800

    register cronos params keys

--- app/app.go
+++ app/app.go
@@ -968,7 +968,7 @@ func initParamsKeeper(appCodec codec.BinaryCodec, legacyAmino *codec.LegacyAmino
 		paramsKeeper.Subspace(gravitytypes.ModuleName)
 	}
 	// this line is used by starport scaffolding # stargate/app/paramSubspace
-	paramsKeeper.Subspace(cronostypes.ModuleName)
+	paramsKeeper.Subspace(cronostypes.ModuleName).WithKeyTable(cronostypes.ParamKeyTable())
 
 	return paramsKeeper
 }
