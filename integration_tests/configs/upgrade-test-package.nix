35610620988852906fd899b9198c51a7d8c171ed
mmsqe <mavis@crypto.com>
2024-04-19 09:22:06 +0800

    try v0.6.5

--- integration_tests/configs/upgrade-test-package.nix
+++ integration_tests/configs/upgrade-test-package.nix
@@ -1,5 +1,6 @@
 let
   pkgs = import ../../nix { };
+  releasedGenesis = import (builtins.fetchTarball "https://github.com/crypto-org-chain/cronos/archive/v0.6.5.tar.gz") { };
   fetchFlake = repo: rev: (pkgs.flake-compat {
     src = {
       outPath = builtins.fetchTarball "https://github.com/${repo}/archive/${rev}.tar.gz";
@@ -7,17 +8,21 @@ let
       shortRev = builtins.substring 0 7 rev;
     };
   }).defaultNix;
+  # v0.7.0
+  released1_0 = (fetchFlake "crypto-org-chain/cronos" "799ac47e293403bd57580d2ff96bb8d9851c3cde").default;
   # v0.8.0
-  releasedGenesis = (fetchFlake "crypto-org-chain/cronos" "2f2cc88b501b47149690fdef05afbbbe5bc116c9").default;
+  released1_1 = (fetchFlake "crypto-org-chain/cronos" "2f2cc88b501b47149690fdef05afbbbe5bc116c9").default;
   # v1.0.15
-  released1_1 = (fetchFlake "crypto-org-chain/cronos" "1f5e2618362303d91f621b47cbc1115cf4fa0195").default;
+  released1_2 = (fetchFlake "crypto-org-chain/cronos" "1f5e2618362303d91f621b47cbc1115cf4fa0195").default;
   # v1.1.1
-  released1_2 = (fetchFlake "crypto-org-chain/cronos" "10b8eeb9052e3c52aa59dec15f5d3aca781d1271").default;
+  released1_3 = (fetchFlake "crypto-org-chain/cronos" "10b8eeb9052e3c52aa59dec15f5d3aca781d1271").default;
   current = pkgs.callPackage ../../. { };
 in
 pkgs.linkFarm "upgrade-test-package" [
   { name = "genesis"; path = releasedGenesis; }
-  { name = "v1.0.0"; path = released1_1; }
-  { name = "v1.1.0"; path = released1_2; }
+  { name = "v0.7.0"; path = released1_0; }
+  { name = "v0.7.0-hotfix"; path = released1_1; }
+  { name = "v1.0.0"; path = released1_2; }
+  { name = "v1.1.0"; path = released1_3; }
   { name = "v1.2"; path = current; }
 ]
