7d7d659c050b095193cb416d9a3fbcdde0249e92
mmsqe <mavis@crypto.com>
2024-12-06 18:25:02 +0800

    api

--- integration_tests/test_authz.py
+++ integration_tests/test_authz.py
@@ -39,16 +39,15 @@ stdout_logfile = %(here)s/node2.log
     time.sleep(1)
     cli = cronos.cosmos_cli()
     wait_for_new_blocks(cli, 1)
-    encoded_tx = "CvYBCroBCh4vY29zbW9zLmF1dGh6LnYxYmV0YTEuTXNnR3JhbnQSlwEKKmNyYzF4N3g5cGtmeGYzM2w4N2Z0c3BrNWFldHdua3IwbHZsdjMzNDZjZBIqY3JjMTZ6MGhlcno5OTg5NDZ3cjY1OWxyODRjOGM1NTZkYTU1ZGMzNGhoGj0KOwomL2Nvc21vcy5iYW5rLnYxYmV0YTEuU2VuZEF1dGhvcml6YXRpb24SEQoPCghiYXNldGNybxIDMjAw+j82Ci8vZXRoZXJtaW50LnR5cGVzLnYxLkV4dGVuc2lvbk9wdGlvbkR5bmFtaWNGZWVUeBIDCgEwEl8KVwpPCigvZXRoZXJtaW50LmNyeXB0by52MS5ldGhzZWNwMjU2azEuUHViS2V5EiMKIQNg/r8Tea2PYFq2XE07fpnN97ASePg32cuO4HmUkEGpFBIECgIIARIEEMCaDBpBqjIjjd67xL2FGigffzzTpmhj4tzdCCoUGe+SIU0/nR8c9yb44BDiK0pmId4z6IOfHR3383EvTa2b/Z+mj3UgJAE="
-    res = requests.post("http://localhost:27007", json=json_rpc_send_body(encoded_tx))
-    print("mm-res", res.json())
-    time.sleep(1200)
-
-
-def json_rpc_send_body(raw, method="broadcast_tx_async"):
-    return {
+    raw = "CvYBCroBCh4vY29zbW9zLmF1dGh6LnYxYmV0YTEuTXNnR3JhbnQSlwEKKmNyYzF4N3g5cGtmeGYzM2w4N2Z0c3BrNWFldHdua3IwbHZsdjMzNDZjZBIqY3JjMTZ6MGhlcno5OTg5NDZ3cjY1OWxyODRjOGM1NTZkYTU1ZGMzNGhoGj0KOwomL2Nvc21vcy5iYW5rLnYxYmV0YTEuU2VuZEF1dGhvcml6YXRpb24SEQoPCghiYXNldGNybxIDMjAw+j82Ci8vZXRoZXJtaW50LnR5cGVzLnYxLkV4dGVuc2lvbk9wdGlvbkR5bmFtaWNGZWVUeBIDCgEwEl8KVwpPCigvZXRoZXJtaW50LmNyeXB0by52MS5ldGhzZWNwMjU2azEuUHViS2V5EiMKIQNg/r8Tea2PYFq2XE07fpnN97ASePg32cuO4HmUkEGpFBIECgIIARIEEMCaDBpBqjIjjd67xL2FGigffzzTpmhj4tzdCCoUGe+SIU0/nR8c9yb44BDiK0pmId4z6IOfHR3383EvTa2b/Z+mj3UgJAE="
+    hash = requests.post("http://localhost:27007", json={
         "jsonrpc": "2.0",
-        "method": method,
+        "method": "broadcast_tx_async",
         "params": {"tx": raw},
         "id": 1,
-    }
+    }).json().get("result").get("hash")
+    print("mm-hash", hash)
+    cronos.cosmos_cli().event_query_tx_for(hash)
+    time.sleep(3)
+    res = requests.get(f"http://localhost:27004/cosmos/tx/v1beta1/txs/{hash}").json()
+    print("mm-res", res["tx_response"])
